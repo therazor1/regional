bb7461944e06e745f4b39b1f3455363aa9663d94
ameraz1 <ameraz@focusit.pe>
2023-09-07 22:19:13 -0500

    Delete crons

--- crons.sh
+++
@@ -1,9 +0,0 @@
-
-# Todos los dias a las 4 am: push a los conductores q aún no han sincronizado (+5) horaUTC
-0 9 * * * curl https://primaxdreams.focusit.pe/api/api/crons/cron_dreams_6veces_diario
-15 9 * * * curl https://primaxdreams.focusit.pe/api/api/crons/cron_dreams_6veces_diario
-30 9 * * * curl https://primaxdreams.focusit.pe/api/api/crons/cron_dreams_6veces_diario
-
-#test saber si funciona 2 min
-*/2 * * * * curl https://primaxdreams.focusit.pe/api/api/crons/cron_dreams_6veces_diario
-
