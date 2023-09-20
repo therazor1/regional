e6b035f6dd1f94d138689665f5a704569fd4df9c
ameraz1 <ameraz@focusit.pe>
2023-09-20 12:50:26 -0500

    Add memoria/getLevel, obstaculo/getLevel

--- controllers/api/memoria.php
+++ controllers/api/memoria.php
@@ -5,6 +5,7 @@ namespace Controllers\api;
 use Inc\Req;
 use Inc\Rsp;
 use Models\Memoria as ModelsMemoria;
+use Libs\Pixie\QB;
 
 class memoria extends _controller {
 
@@ -23,6 +24,19 @@ class memoria extends _controller {
         $upLevel = new ModelsMemoria($data->id_user);
         return Rsp::ok()
                 ->set('status', $upLevel->state);
+    }
+
+    public function getLevel(Req $req){
+        $data = $req->data([
+            'id_user' => 'required'
+        ]);
+
+        $qb = QB::table('memoria')->select(['nivel'])->where('id_user', $data->id_user)->get()[0];
+        return Rsp::ok()
+                ->set('ok', true)
+                ->set('msg', "Nivel actual del juego memoria")
+                ->set('nivel', $qb->nivel);
+
     }
 
 }
--- controllers/api/obstaculo.php
+++ controllers/api/obstaculo.php
@@ -5,6 +5,7 @@ namespace Controllers\api;
 use Inc\Req;
 use Inc\Rsp;
 use Models\Obstaculo as ModelsObstaculo;
+use Libs\Pixie\QB;
 
 class obstaculo extends _controller {
 
@@ -23,6 +24,19 @@ class obstaculo extends _controller {
         $upLevel = new ModelsObstaculo($data->id_user);
         return Rsp::ok()
                 ->set('status', $upLevel->state);
+    }
+
+    public function getLevel(Req $req){
+        $data = $req->data([
+            'id_user' => 'required'
+        ]);
+
+        $qb = QB::table('obstaculo')->select(['nivel'])->where('id_user', $data->id_user)->get()[0];
+        return Rsp::ok()
+                ->set('ok', true)
+                ->set('msg', "Nivel actual del juego obstaculo")
+                ->set('nivel', $qb->nivel);
+
     }
 
 }
